ff1f8b82519ebf523a18bed5ef018dde264acbc8
varshini <v.krishnasrivarshini@gmail.com>
2025-11-11 11:52:27 +0530

    Fix fahrenheit calculation in temperature conversion

--- temperature_convo.cpp
+++ temperature_convo.cpp
@@ -5,6 +5,7 @@ int main(){
   float temp;
   cout<<"enter the temperature celsius"<<endl;
   cin>>temp; 
-  cout<<"temp in fahrenhiet ="<<(temp*(9/5))+32<<endl;
+  cout<<"temp in fahrenhiet ="<<((temp*9)/5)+32<<endl;
   return 0;
+
 }
