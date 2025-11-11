cc607afbc1ac44b7640e3e194707b379db035f2a
varshini <v.krishnasrivarshini@gmail.com>
2025-11-11 11:32:52 +0530

    temperature conversion using c++

---
+++ temperature_convo.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  float temp;
+  cout<<"enter the temperature celsius"<<endl;
+  cin>>temp; 
+  cout<<"temp in fahrenhiet ="<<(temp*(9/5))+32<<endl;
+  return 0;
+}
