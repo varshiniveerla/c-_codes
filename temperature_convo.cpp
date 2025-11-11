d2e01e6a3761e982eb5d0c25630ec513461e9f4a
varshini <v.krishnasrivarshini@gmail.com>
2025-11-11 12:02:56 +0530

    Add temperature conversion from Celsius to Fahrenheit

---
+++ temperature_convo.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  float temp;
+  cout<<"Enter the temperature celsius";
+  cin>>temp; 
+  cout<<"Temp in fahrenhiet = "<<((temp*9.0)/5.0)+32<<endl;
+  return 0;
+}
